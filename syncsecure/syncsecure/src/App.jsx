54811ba361d4611217b4418ed2ac222fee3906b8
thanish076 <143092871+thanish076@users.noreply.github.com>
2023-08-29 13:59:19 +0530

    Add files via upload

---
+++ syncsecure/syncsecure/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="dashboard">
+      <div className='row'>
+        <div className='col-md-2 control'>
+          <button className="button">Account</button>
+          <button className="button">File Share</button>
+          <button className="button">Connect</button>
+          <button className="button">Reports</button>
+          <button className="button">Store</button>
+        </div>
+
+        <div className='col-md-10 second'>
+          <h1>Hello</h1>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App
